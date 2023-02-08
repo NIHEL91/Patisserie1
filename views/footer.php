6e20eb8b9478986659660b55f61208dd01e3ee1c
Ghoul Nihel <nihel.elghoul@gmail.com>
2023-02-08 22:07:00 +0100

    08022023

--- views/footer.php
+++ views/footer.php
@@ -1,13 +1,13 @@
 <footer>
-        <div class=" lg bg-dark text-white  p-4 mt-5">
+        <div class=" foot-lg bg-dark text-white  p-4 mt-5">
                 <div class="row border-bottom pb-3">
                     <div class="col-4" >
-                        <h5>LA BOULANGERIE</h5>
+                        LA BOULANGERIE
                         <p>S'ABONNER</p>
                         <input type="mail" name="mail" placeholder="Adresse mail" class=""> 
                     </div>
                     <div class="col-4">
-                        <h5>BESOIN D'AIDE</h5>
+                        BESOIN D'AIDE
                         <a href="contact.php" class="text-white p-2" target="_self">Nous contacter</a>
                         <br>
                         <a href="" class="text-white p-2" target="_self">Questions fréquentes</a>
@@ -15,7 +15,6 @@
                         <a href="" class="text-white p-2" target="_self">Commandes</a>
                     </div>
                     <div class="col-4">
-                        <h5> LA BOULANGERIE</h5>
                         <a href="" class="text-white p-2" target="_self">Ou nous trouver?</a>
                         <br>
                         <a href="" class="text-white p-2" target="_self">Qui sommes-nous?</a>
@@ -25,15 +24,11 @@
                 </div>                    
                <div class="row pt-3">
                     <div class="col-lg-4 col-md-8 col-sm-6 float-left">
-                        <h6 class="copyright text-white">Copyright &copy;2023 / Tous droits réservés. </h6>
+                        Copyright &copy2023 / Tous droits réservés. 
                     </div>
                     <div class="col-lg-8 col-md-4 col-sm-6 float-lg-right">
                         <div class="foot_menu">
-                            <ul>
-                            <a href="" class="text-white p-2" target="_self">Politique de confidentialité</a>
-                            <a href="" class="text-white p-2" target="_self">Paiement sécurisé</a> 
-                            <a href="index.php?entite=mentions?action=" class="text-white p-2" target="_self">Mentions légales</a> 
-                            </ul>
+                           
                         </div>
                     </div>
                 </div>
--- views/header.php
+++ views/header.php
@@ -1,36 +1,35 @@
 
 <header>
-<nav class="navbar navbar-expand-lg  bg-dark justifi">
+<nav class="navbar navbar-expand-lg navbar-light bg-light ">
         <div class="container-fluid">
-            <img src="../img/logoo.png" class="logo"/>
+            <img src="../img/logo.png" class="logo"/>
 
-            <a class="navbar-brand text-white px-3" href="#">Accueil</a>
+            <a class="navbar-brand  px-3" href="#"><h5>Accueil</h5></a>
             <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon "></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                <a class="nav-link active text-white  px-3" aria-current="page" href="#">Anniversaires</a>
+                <a class="nav-link active   px-3" aria-current="page" href="#"><h5>Anniversaires</h5></a>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link text-white  px-3" href="#">Mariage</a>
+                <a class="nav-link   px-3" href="#">Mariage</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle text-white  px-3" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle   px-3" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Gateaux aux thémes
                     </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <li><a class="dropdown-item" href="#">Princesses><img src="../img/cake_logo.jpeg"></a></li>
                             <li><a class="dropdown-item" href="#">Football</a></li>
-                            <li><hr class="dropdown-divider"></li>
                             <li><a class="dropdown-item" href="#">Coeur</a></li>
                             <li><a class="dropdown-item" href="#">Fleurs</a></li>
                             <li><a class="dropdown-item" href="#">Amours</a></li>
                         </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white  px-3" href="#" >Contacter</a>
+                    <a class="nav-link   px-3" href="#" >Contacter</a>
                 </li>
             </ul>
             <form class="d-flex">
@@ -45,7 +44,7 @@
                     echo'<span> <a href="controllers/compte.php"  class="btn  btn-sm me-2 "><img src="../img/carré-jaune.png" class="compte"/></a></span>';
                 //echo '<span><a href="controllers/connect.php" class="btn btn-outline-warning ">Créer un compte</a>';
                 } ?>
-                <a href="#"  class="btn  btn-sm me-2 "><img src="../img/panierimage.png" class="panier"/></a>
+                <a href="#"  class="btn  btn-sm me-2 "><img src="../img/panier.png" class="panier"/></a>
                 
                 <span class="badge badge-secondary text-warning"> <?php echo date('l d.m.Y'); ?></span>
                 <!--<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
